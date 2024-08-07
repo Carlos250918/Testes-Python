05d46e334478f3958787a8fe74fb0edd601e0bc7
Carlos Martinati <carlosalbmartinati@gmail.com>
2024-08-06 21:45:28 -0300

    Update automacao_selenium.py

--- automacao_selenium.py
+++ automacao_selenium.py
@@ -6,7 +6,7 @@ chrome = webdriver.Chrome()
 chrome.get('https://books.toscrape.com/')
 sleep(1)
 
-titulo = chrome.find_element(By.XPATH,"../../media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg" alt="A Light in the Attic">)
+titulo = chrome.find_element(By.XPATH,'../../media/cache/fe/72/fe72f0532301ec28892ae79a629a293c.jpg' alt="A Light in the Attic">)
 print(titulo.get_attribute('title'))
 
 sleep(10)
